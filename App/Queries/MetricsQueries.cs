6e0bbb085ff2b0ae7a76784bc4a886718a826f01
Aymen TROUDI <aymen.troudi@younited-credit.fr>
2020-03-01 13:19:23 +0100

    Customize Timespan/Interval

--- App/Queries/MetricsQueries.cs
+++ App/Queries/MetricsQueries.cs
@@ -4,10 +4,13 @@ namespace App.Queries
 {
     public static class MetricsQueries
     {
-        public static string RequestsNumberQuery = "metrics/requests/count?timespan=PT2H&interval=PT30M";
-        public static string RequestsDurationQuery = "metrics/requests/duration?timespan=PT2H&interval=PT30M";
-        public static string RequestsFailedQuery = "metrics/requests/failed?timespan=PT2H&interval=PT30M";
-        public static string ExceptionsNumberQuery = "metrics/exceptions/count?timespan=PT2H&interval=PT30M";
+        private const int Timespan = 30;
+        private const int Interval = 10;
+
+        public static string RequestsNumberQuery = $"metrics/requests/count?timespan=PT{Timespan}M&interval=PT{Interval}M";
+        public static string RequestsDurationQuery = $"metrics/requests/duration?timespan=PT{Timespan}M&interval=PT{Interval}M";
+        public static string RequestsFailedQuery = $"metrics/requests/failed?timespan=PT{Timespan}M&interval=PT{Interval}M";
+        public static string ExceptionsNumberQuery = $"metrics/exceptions/count?timespan=PT{Timespan}M&interval=PT{Interval}M";
 
         public static IDictionary<string, string> All = new Dictionary<string, string>
         {
